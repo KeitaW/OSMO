6472135cc37380cd5fe35c4552446e29d36c7b7d
Keita Watanabe <keitaw09@gmail.com>
2026-04-01 23:40:10 +0000

    debug: add exec logging to osmo_exec

--- src/runtime/cmd/user/user.go
+++ src/runtime/cmd/user/user.go
@@ -419,6 +419,7 @@ func main() {
 		if response.Type == messages.CtrlFailed {
 			return
 		} else if response.Type == messages.ExecStart {
+			log.Printf("ExecStart received, command=%s args=%v", cmdArgs.Command, cmdArgs.Args)
 			break
 		} else {
 			log.Printf("Ignore unexpected Type: %s", response.Type)
@@ -467,6 +468,7 @@ func main() {
 		go data.Checkpoint(opsChan, checkpoint, &waitCheckpoint, &stopCheckpoint)
 	}
 	waitUserCommands.Wait()
+	log.Printf("User command finished: msg=%q err=%v", cmdMsg, cmdErr)
 	execFinished = true
 	stopCheckpoint = true
 	waitCheckpoint.Wait()
